394edd2a637717828cd74e800decb96fbcb66506
dprimedx <dprimedx@gmail.com>
2010-03-19 15:12:06 -0400

    Tests ID values existance

--- TaskListTest.py
+++ TaskListTest.py
@@ -2,10 +2,10 @@ from Task import Task
 from TaskList import TaskList
 
 tl = TaskList()
-tl.add(Task("Walk the dog"))
-tl.add(Task("Clean desk",1))
-tl.add(Task("Wash sheets",0))
-tl.add(Task("Do homework",2))
+tl.add(Task(0,"Walk the dog"))
+tl.add(Task(0,"Clean desk",1))
+tl.add(Task(0,"Wash sheets",0))
+tl.add(Task(0,"Do homework",2))
 
-for x in tl.getByPriority():
-	print(x.desc)
+for x in tl.getItems():
+	print(x.id,x.desc)
