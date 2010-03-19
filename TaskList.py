7bebdb13f9fb14e60b455f493f45ace2946b90c8
dprimedx <dprimedx@gmail.com>
2010-03-19 15:11:45 -0400

    Adds ID value to Task

--- TaskList.py
+++ TaskList.py
@@ -1,11 +1,17 @@
 class TaskList:
 	def __init__(self):
 		self.items = []
+		self.currentID = 0
+
 	def add(self,item):
 		if item is not None:
+			self.currentID += 1
+			item.id = self.currentID
 			self.items.append(item)
+
 	def getItems(self):
 		return self.items
+
 	def getByPriority(self,priority=None):
 		if priority is None:
 			return self.items
@@ -14,3 +20,9 @@ class TaskList:
 			if x.priority == priority:
 				result.append(x)
 		return result
+
+	def getByID(self,id=-1):
+		if id > -1:
+			for x in self.items:
+				if x.id == id:
+					return x
